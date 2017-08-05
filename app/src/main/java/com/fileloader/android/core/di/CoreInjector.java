2ec5dbd40fe6e43178b48b123769f9c76a66675e
dhara shah <hearbeat.horror@gmail.com>
2017-08-05 12:11:45 +0700

    remove un-necessary method

--- app/src/main/java/com/fileloader/android/core/di/CoreInjector.java
+++ app/src/main/java/com/fileloader/android/core/di/CoreInjector.java
@@ -1,13 +1,8 @@
 package com.fileloader.android.core.di;
 
-import android.app.Fragment;
 import android.content.Context;
 
 public class CoreInjector {
-    public static CoreComponent from(Fragment fragment) {
-        return from(fragment.getActivity());
-    }
-
     @SuppressWarnings("unchecked")
     public static CoreComponent from(Context context) {
         CoreComponentProvider provider = (CoreComponentProvider) context.getApplicationContext();
